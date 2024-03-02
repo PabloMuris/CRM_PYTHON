e8dc959c28f2e47242f3863675f548cd3acf85d4
Pablo Murilo <pablo.murilo10@hotmail.com>
2024-03-02 20:55:40 -0300

    segunda parte

--- website/forms.py
+++ website/forms.py
@@ -15,3 +15,19 @@ class SignupForm(UserCreationForm):
 
         def __init__(self,*args,**kwargs):
             super(SignupForm,self).__init__(*args,**kwargs)
+
+            self.fields['username'].widget.attrs['class'] = 'form-control'
+            self.fields['username'].widget.attrs['placeholder'] = 'Usuário'
+            self.fields['username'].label = ''
+            self.fields['username'].help_text = '<span class="form-text text-muted"><small>Necessário 150 characters ou menos. Letras, digitos e @/./+/-/_ only.</small></span>'
+
+            self.fields['password1'].widget.attrs['class'] = 'form-control'
+            self.fields['password1'].widget.attrs['placeholder'] = 'Senha'
+            self.fields['password1'].label = ''
+            self.fields['password1'].help_text = '<ul class="form-text text-muted small"><li>Sua senha não deve conter informações pessoais.</li><li>Sua senha deve ter no minimo 8 digitos</li><li>Evite senhas muito comuns</li><li>Senhas que tenham apenas número snão serão aceitas.</li></ul>'
+
+            self.fields['password2'].widget.attrs['class'] = 'form-control'
+            self.fields['password2'].widget.attrs['placeholder'] = 'Confirme a senha'
+            self.fields['password2'].label = ''
+            self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Digite o mesmo email, para verifficação</small></span>'	
+
--- website/views.py
+++ website/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 from .forms import SignupForm
+from .models import Record
 
 def home(request):
+
+    records = Record.objects.all()
+     
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -23,13 +27,23 @@ def logout_user(request):
     logout(request)
     messages.success(request, ' você fez logout')
     return redirect('home')
-def register_user(request):
-    if request.method == 'POST':
-        form = SignupForm(request.POST)
-        if form.is_valid():
-            form.save()
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password1']
-            user = authenticate
 
-    return render(request,'register.html',{})
+
+def register_user(request):
+	if request.method == 'POST':
+		form = SignupForm(request.POST)
+		if form.is_valid():
+			form.save()
+			# Authenticate and login
+			username = form.cleaned_data['username']
+			password = form.cleaned_data['password1']
+			user = authenticate(username=username, password=password)
+			login(request, user)
+			messages.success(request, "Você entrou. Bem-vindo")
+			return redirect('home')
+	else:
+		form = SignupForm()
+		return render(request, 'register.html', {'form':form})
+
+	return render(request, 'register.html', {'form':form})
+ 
